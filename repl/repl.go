03d9d42d02e133d537cc2366fa6e5de28906dad9
Brandon Charest <brandon.charest@gmail.com>
2024-03-15 15:00:02 -0400

    repl

---
+++ repl/repl.go
@@ -0,0 +1,27 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey-go/lexer"
+	"monkey-go/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Print(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		lexer := lexer.New(line)
+		for tok := lexer.NextToken(); tok.Type != token.EOF; tok = lexer.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
